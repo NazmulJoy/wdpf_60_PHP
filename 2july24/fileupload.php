f2004d813dd702c391853c10f970e54c5957e925
NazmulJoy <Nazmuljoys@gmail.com>
2024-07-02 17:03:50 +0600

    2july24

--- 2july24/fileupload.php
+++ 2july24/fileupload.php
@@ -3,55 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>File Upload Form</title>
 </head>
 <body>
     <?php 
     if(isset($_POST["submit"])){
 
-    $temp = $_FILES["homework"]["tmp_name"];
-     $filename = $_FILES["homework"]["name"];
-     $filedata = pathinfo($filename, PATHINFO_EXTENSION);
-     strtolower($filedata);
-     $allowedext = ["jpg", "gif", "jpeg"];
-     $filesize = $_FILES["homework"]["size"];
-     $allowedsize = 307200;
-    $path = "hw/". $filename;
-    $errors = [];
-    if($filesize>$allowedsize){
-        $errors[] = "File size must be less than 300Kb";
-    } if(!in_array($filedata,$allowedext)){
-        $errors[] = $filedata." format not allowed";
-    } 
-    
-    
-    if(count($errors)>0){
-        foreach($errors as $err){
-            echo $err. "<br>";
+        // Retrieve file details
+        $temp = $_FILES["homework"]["tmp_name"];
+        $filename = $_FILES["homework"]["name"];
+        $filedata = strtolower(pathinfo($filename, PATHINFO_EXTENSION));  // Get file extension and convert to lowercase
+        $allowedext = ["jpg", "gif", "jpeg"];  // Allowed file extensions
+        $filesize = $_FILES["homework"]["size"];
+        $allowedsize = 307200;  // Maximum allowed file size (300KB)
+        $path = "hw/" . $filename;  // Destination path for uploaded file
+        $errors = [];
+
+        // Check file size
+        if($filesize > $allowedsize){
+            $errors[] = "File size must be less than 300KB";
         }
-    } else {
-        $status = move_uploaded_file( $temp, $path );
-        if($status){
-            echo "Successfully Uploaded";
+
+        // Check file extension
+        if(!in_array($filedata, $allowedext)){
+            $errors[] = $filedata . " format not allowed";
+        }
+
+        // Display errors or move uploaded file
+        if(count($errors) > 0){
+            foreach($errors as $err){
+                echo $err . "<br>";
+            }
+        } else {
+            $status = move_uploaded_file($temp, $path);
+            if($status){
+                echo "Successfully Uploaded";
+            } else {
+                echo "File upload failed";
+            }
         }
     }
-     
-    }
-    
-    
     ?>
-<form action="" method="post" enctype="multipart/form-data">
-    
-    <label form="name">Name:</label><br>
-    <input type="text" name="name" value=""><br>
-    <label form="email">Email:</label><br>
-    <input type="text" name="email" value=""><br>
-    <label form="homework">Class notes:</label>
-    <input type="file" name="homework" value=""><br>
-    <input type="submit" name="submit" value="Submit Homework">
+
+    <!-- HTML Form -->
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="name">Name:</label><br>
+        <input type="text" name="name" value=""><br>
+        <label for="email">Email:</label><br>
+        <input type="text" name="email" value=""><br>
+        <label for="homework">Class notes:</label>
+        <input type="file" name="homework" value=""><br>
+        <input type="submit" name="submit" value="Submit Homework">
     </form>
 </body>
 </html>
+
 
 
 
